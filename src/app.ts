0e8f6fe455591b4facd7d332848a5a05efaf7ac0
Jean-Vincent KASSI <jekas@smile.ci>
2020-02-04 14:08:35 +0000

    Cleaning up

--- src/app.ts
+++ src/app.ts
@@ -2,61 +2,66 @@ import * as faceapi from "face-api.js";
 // import { Context } from "vm";
 import "./app.css";
 export class App {
-  // context: canvas.;
   private canvas: HTMLCanvasElement;
-  // private context: CanvasRenderingContext2DSettings;
   private videoElement: HTMLVideoElement;
+  // 60 images per seconds
   private FPS: number = 60;
-  private expression: string = "";
   context: CanvasRenderingContext2D;
+  private faceapi = faceapi;
 
   async detectFace() {
     const canvas = this.canvas;
     const context = this.context;
-
     context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
-    const detections = await faceapi
-      .detectAllFaces(
-        //  this.videoElement,
+
+    let detection = await faceapi
+      // .detectAllFaces(
+      .detectSingleFace(
         canvas,
-        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.05 })
+        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.1 })
       )
-      .withFaceLandmarks()
+      // .withFaceLandmarks()
       .withFaceExpressions();
 
-    if (detections.length == 0) return;
+      // if no face found try again
+    if (!detection) {
+      setTimeout(() => this.detectFace(), 1000 / this.FPS);
+      return;
+    }
+
     const displaySize = {
       width: canvas.width,
       height: canvas.height
     };
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
-    resizedDetections.map(detection => {
-      this.expression = detection.expressions.asSortedArray()[0].expression;
-      let box = detection.detection.box;
-      let emoji = new Image();
-      emoji.src = `/emojis/${this.expression}.png`;
-      //  this.draw();
-      context.drawImage(
-        emoji,
-        0,
-        0,
-        512,
-        512,
-        box.x,
-        box.y,
-        box.width + 5,
-        box.height + 5
-      );
-    });
+    detection = faceapi.resizeResults(detection, displaySize);
+    let expression = detection.expressions.asSortedArray()[0].expression;
+    let box = detection.detection.box;
+    this.drawEmoji(box, expression);
+    setTimeout(() => this.detectFace(), 1000 / this.FPS);
   }
-  async draw(canvas, context) {
-    context.drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
+
+  async drawEmoji(box, expression) {
+    let emoji = new Image();
+    emoji.src = `/emojis/${expression}.png`;
+    this.context.drawImage(
+      emoji,
+      0,
+      0,
+      512,
+      512,
+      box.x,
+      box.y,
+      box.width,
+      box.height
+    );
   }
 
   public async attached() {
     await this.loadModel();
     this.canvas = document.querySelector("canvas");
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
 
     this.getMediaStream().then((stream: MediaStream) => {
@@ -72,7 +77,7 @@ export class App {
         "play",
         async () => {
           console.log("play");
-          setInterval(() => this.detectFace(), 1000 / this.FPS);
+          this.detectFace();
         },
         false
       );
@@ -102,30 +107,28 @@ export class App {
     });
   }
 
-  public getCurrentImage(): string {
-    const canvas: HTMLCanvasElement = document.createElement("canvas");
-    document.body.appendChild(canvas);
+  // public getCurrentImage(): string {
+  //   const canvas: HTMLCanvasElement = document.createElement("canvas");
+  //   document.body.appendChild(canvas);
 
-    const width: number = this.videoElement.clientWidth;
-    const height: number = this.videoElement.clientHeight;
-    canvas.width = width;
-    canvas.height = height;
-    const context: CanvasRenderingContext2D = canvas.getContext("2d");
-    context.drawImage(this.videoElement, 0, 0, width, height);
-    const dataURL: string = canvas.toDataURL("image/png");
-    document.body.removeChild(canvas);
-    //console.log(dataURL);
-    return dataURL;
-  }
+  //   const width: number = this.videoElement.clientWidth;
+  //   const height: number = this.videoElement.clientHeight;
+  //   canvas.width = width;
+  //   canvas.height = height;
+  //   const context: CanvasRenderingContext2D = canvas.getContext("2d");
+  //   context.drawImage(this.videoElement, 0, 0, width, height);
+  //   const dataURL: string = canvas.toDataURL("image/png");
+  //   document.body.removeChild(canvas);
+  //   return dataURL;
+  // }
 
   async loadModel() {
-    // await faceapi.loadFaceExpressionModel("/models");
+    // console.log(Object.keys(faceapi.nets.ssdMobilenetv1));
+    // console.log(faceapi.nets.ssdMobilenetv1.isLoaded);
     await faceapi.loadSsdMobilenetv1Model("/models");
     await faceapi.loadFaceDetectionModel("/models");
-    await faceapi.loadFaceLandmarkModel("/models");
+    // await faceapi.loadFaceLandmarkModel("/models");
     await faceapi.loadFaceExpressionModel("/models");
     // await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
-
-    //console.log(faceapi.nets);
   }
 }
